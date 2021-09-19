2deff69168fb6398c601c2da8ebe0553cd06677e
Julio Cesar Froes de Oliveira <froees147@gmail.com>
2021-09-19 18:49:22 -0300

    Quinta lista

---
+++ icc_5/erro_pt.py
@@ -0,0 +1,8 @@
+"""
+Seu sobrinho Joberto está aprendendo a escrever e está tendo 
+dificuldade de lembrar que deve usar letra maiúscula no início da frase.
+
+Como um bom tio que é, e um exímio 
+programador, você decide que a melhor 
+abordagem é escrever um programa que faça as correções automaticamente."""
+
