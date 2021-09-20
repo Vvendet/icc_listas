bf95410a43aa166dff5035c42558e2abc9e828fc
Julio Cesar Froes de Oliveira <froees147@gmail.com>
2021-09-20 18:08:38 -0300

    atualizando quinta lista

--- icc_5/erro_pt.py
+++ icc_5/erro_pt.py
@@ -6,3 +6,16 @@ Como um bom tio que é, e um exímio
 programador, você decide que a melhor 
 abordagem é escrever um programa que faça as correções automaticamente."""
 
+texto = input().split('.')
+texto = [list(texto[0]),list(texto[1])]
+formated_text = []
+for i in range(0,len(texto)):
+    for j in texto[i]:
+        if (j != ''):
+            print(j)
+            formated_text.append(j.upper())
+            texto[i].remove(j)
+            break
+        else:
+            formated_text.append(j)
+            texto[i].remove(j)
